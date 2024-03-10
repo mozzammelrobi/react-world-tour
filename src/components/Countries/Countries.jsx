c6ea0e91a0ce52de42d6dec9d75b0d0807769dcb
mozzammelrobi <mozzammelfb@gmail.com>
2024-03-11 02:41:35 +0600

    button click toggle and change going to visited

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -5,18 +5,20 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries]= useState([])
+
     useEffect(()=> {
         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data =>setCountries(data))
     },[])
+
     return (
         <div>
             <h3>Counteris: {countries.length}</h3>
            <div className="country-container">
            {
                 countries.map(country => <Country 
-                    key={country.cca3} 
+                    key={country.cca3}
                     country={country}></Country>)
             }
            </div>
